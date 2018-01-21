df92cd52fa5a2bba9ae069f9347227e8f4a5fc39
Durellg <durellgill@gmail.com>
2018-01-21 16:57:34 -0600

    some api stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+//openweather key: 898b69392d37088b5c01a7c774335e37
+
+var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
+var apiKey = "898b69392d37088b5c01a7c774335e37";
+
+var url = weatherURL + apiKey;
+
+
+console.log(_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 }));
+
+
