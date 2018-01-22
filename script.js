5252cdf4dc37531f7b4cad8b96479d0e1eb2de4c
Durellg <durellgill@gmail.com>
2018-01-22 10:17:19 -0600

    consolidating html

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ var apiKey = "898b69392d37088b5c01a7c774335e37";
 
 var url = weatherURL + apiKey;
 
-
-
-
+/*
+{dt: 1516579200,
+    main: {
+        temp: 266.6,
+        temp_min: 266.254,
+        temp_max: 266.6,
+        pressure: 1003.19,
+        sea_level: 1024.44,
+        grnd_level: 1003.19,
+        humidity: 92,
+        temp_kf: 0.35
+    },
+    weather: [{
+        id: 600,
+        main: "Snow",
+        description: "light snow",
+        icon: "13n"
+    }],
+    clouds: {
+        all: 88
+    },
+    wind: {
+        speed: 2.72,
+        deg: 300.503
+    },
+    snow: {
+        3 h: 0.155
+    },
+    sys: {
+        pod: "n"
+    },
+    dt_txt: "2018-01-22 00:00:00"
+},
+*/
