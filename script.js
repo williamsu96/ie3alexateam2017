941858ff51949fbe2fe799b3a7408c4f3258800f
Durellg <durellgill@gmail.com>
2018-01-27 22:04:45 -0600

    html css and js for mirror

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //openweather key: 898b69392d37088b5c01a7c774335e37
 
-var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
+var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=60201&APPID=";
 var apiKey = "898b69392d37088b5c01a7c774335e37";
 
 var url = weatherURL + apiKey;
+
+window.onload = function () {
+    
+    var date = new moment();
+    var dayInMilliseconds = 1000 * 60 * 60 * 12;
+
+    var updateTime = setInterval(function () {  
+       //var time_display = $("#time").text(time.format("hh:mm:ss a"));
+       var time = new moment();
+       var time_display = document.getElementById("time").innerHTML = time.format("hh:mm a");
+    }, 500);
+
+    var updateTime = setInterval(function () {  
+        var date_display = $("#date").text(date.format("dddd, MMM Do"));
+     }, dayInMilliseconds);
+
+    $("#todayandtemp").click(function() {
+        $("#lineandupcoming").toggle();
+    })
+
+
+}
+
 
 /*
 {dt: 1516579200,
