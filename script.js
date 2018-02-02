147a7109381209b43de8a7edb35e29567f3166da
Durellg <durellgill@gmail.com>
2018-02-02 14:08:12 -0600

    data setup is JS

--- script.js
+++ script.js
@@ -6,86 +6,92 @@ var apiKey = "898b69392d37088b5c01a7c774335e37";
 var url = weatherURL + apiKey;
 
 window.onload = function () {
-    
+
     var date = new moment();
     var dayInMilliseconds = 1000 * 60 * 60 * 12;
 
-    var updateTime = setInterval(function () {  
-       //var time_display = $("#time").text(time.format("hh:mm:ss a"));
-       var time = new moment();
-       var time_display = document.getElementById("time").innerHTML = time.format("hh:mm a");
+    var updateTime = setInterval(function () {
+        //var time_display = $("#time").text(time.format("hh:mm:ss a"));
+        var time = new moment();
+        var time_display = document.getElementById("time").innerHTML = time.format("hh:mm a");
     }, 500);
 
-    var updateTime = setInterval(function () {  
+    var updateTime = setInterval(function () {
         var date_display = $("#date").text(date.format("dddd, MMM Do"));
-     }, dayInMilliseconds);
+    }, dayInMilliseconds);
 
-    $("#todayandtemp").click(function() {
+    $("#todayandtemp").click(function () {
         $("#lineandupcoming").toggle();
     })
 
-    var icons = ["<>", "app", "sdf"];
+    var weatherIcons = {
+        "Thunderstorm": '<i class="small-icon wi wi-storm-showers"></i>',
+        "Clear": '<i class="small-icon wi wi-day-sunny"></i>',
+        "Clouds": '<i class="small-icon wi wi-cloud"></i>',
+        "Drizzle": '<i class="small-icon wi wi-rain"></i>',
+        "Rain": '<i class="small-icon wi wi-rain"></i>',
+        "Snow": '<i class="small-icon wi wi-snow"></i>',
+        "Atmosphere": '<i class="small-icon wi wi-dog"></i>',
+    }
     //console.log(data.city.name);
     var list = data.list;
-    
-    var hourly3 = list.filter((date) => {
-        let timeW = moment(date.dt_txt);
-        //console.log(timeW.format("dddd"));
-        //console.log(moment().format("dddd"))
-        timeW.format("dddd") == moment().format("dddd");
-    })
-    console.log(hourly3);
-    
-    
-    var five_day = _.each(list, function (value) {
-        let timeW = moment(value.dt_txt);
-        console.log( timeW.format("ddd, MMM Do, hh:mm a"));
-    })
-    
 
-    function getWeatherIcon(weather) {
-        
+    var hourly3 = getThreeHours(list);
+    var five_day = getFiveDays(list);
+    console.log(hourly3);
+    //console.log(five_day);
+
+    var five_day_objects = makeWeatherObjects(five_day);
+    console.log(five_day_objects);
+
+
+    function getThreeHours(array) {
+        newArr = array.filter((date) => {
+            let timeW = moment(date.dt_txt);
+            //console.log(timeW.format("dddd"));
+            //console.log(moment().format("dddd"))
+            return timeW.format("dddd") == moment().format("dddd");
+        })
+        return newArr;
+    }
+
+    function getFiveDays(array) {
+        newArr = array.filter((value) => {
+            let timeW = moment(value.dt_txt);
+            //console.log( timeW.format("ddd, MMM Do, hh:mm a"));
+            //console.log(moment().format("hh"));
+
+            return timeW.format("HH") == "12";
+        })
+        newArr.shift();
+        return newArr;
+    }
+
+    function makeWeatherObjects(arr) {
+        newArr = arr.map((data) => {
+            return new Weather(data);
+        })
+
+        return newArr;
+    }
+
+    function Weather(data) {
+        this.time = data.dt_txt;
+        this.temp = data.main.temp;
+        this.lo = data.main.temp_min;
+        this.hi = data.main.temp_max;
+        this.weather = data.weather[0].main;
+        this.icon = weatherIcons[this.weather];
+        //this.weather = weatherIcons['data.weather[0].main']
     }
 
 
 
-    
+
+
+
+
+
 
 
 }
-
-
-/*
-{dt: 1516579200,
-    main: {
-        temp: 266.6,
-        temp_min: 266.254,
-        temp_max: 266.6,
-        pressure: 1003.19,
-        sea_level: 1024.44,
-        grnd_level: 1003.19,
-        humidity: 92,
-        temp_kf: 0.35
-    },
-    weather: [{
-        id: 600,
-        main: "Snow",
-        description: "light snow",
-        icon: "13n"
-    }],
-    clouds: {
-        all: 88
-    },
-    wind: {
-        speed: 2.72,
-        deg: 300.503
-    },
-    snow: {
-        3 h: 0.155
-    },
-    sys: {
-        pod: "n"
-    },
-    dt_txt: "2018-01-22 00:00:00"
-},
-*/
