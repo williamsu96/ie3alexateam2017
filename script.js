d8a1c5bec66b031aa6a6bb945fa9dff68941841c
Durellg <durellgill@gmail.com>
2018-03-01 02:19:44 -0600

    calendar working but few bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //openweather key: 898b69392d37088b5c01a7c774335e37
 
+
 var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=Chicago&id=60201&units=imperial&APPID=";
 var apiKey = "898b69392d37088b5c01a7c774335e37";
 
@@ -13,6 +14,7 @@ var weatherServer = "https://alexa-magic-mirror.herokuapp.com/display/current";
 var forecastServer = "https://alexa-magic-mirror.herokuapp.com/display/forecast";
 var flagsServer = "https://alexa-magic-mirror.herokuapp.com/display/getflag";
 //flags: "current" / "forecast"/ "hide" / "calendar";
+
 
 window.onload = function () {
 
@@ -136,8 +138,24 @@ window.onload = function () {
             }
         }
 
+        function showCalendar() {
+            for (let i = 0; i < myEvents.length; i++) {
+                displayCalendar(myEvents[i]);
+            }
+        }
+
+        function displayCalendar(calEvent) {
+            //var when = event.start.dateTime;
+            var when = moment(calEvent.start.dateTime).format("hh:mm a");
+            var summary = calEvent.summary;
+            $('#Calendar').append(`<div> ${when} ${summary} </div>`);
+        }
+
+
+
         showDaily();
         showHourly();
+        showCalendar();
         $('#futureDays').hide();
         $('#futureTimes').hide();
         $('#Calendar').hide();
@@ -182,6 +200,8 @@ window.onload = function () {
         }
         var state = 1;
         $("#todayandtemp").click(function () {
+            console.log(myEvents);
+
             toggle(state);
             state++;
             if (state >= 4) state = 0;
@@ -253,4 +273,118 @@ window.onload = function () {
                 break;
         }
     }
+
+}
+
+var CLIENT_ID = '123443630640-hod7nh8d4ra5vrv49nb1irodmi0d5ap8.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBorTMiHFRzdz6v8ekiVG02EVNzOAiA0gY';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        $('#spooky').hide();
+        listUpcomingEvents();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+
+var myEvents = [];
+
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 5,
+        'orderBy': 'startTime'
+    }).then(function (response) {
+        var events = response.result.items;
+        console.log(events);
+
+
+
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime;
+                if (!when) {
+                    when = event.start.date;
+                }
+                console.log(event.summary)
+                console.log(moment(when).format("hh:mm a"));
+                myEvents.push(event)
+            }
+        } else {
+            console.log('No upcoming events found.');
+        }
+    });
 }
