aea3c5c8c4d7a244dca4c3f68256f3369d553a1f
Durellg <durellgill@gmail.com>
2018-03-01 23:11:42 -0600

    ahhhhh

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'esversion: 6';
 //openweather key: 898b69392d37088b5c01a7c774335e37
 
 
@@ -5,10 +6,10 @@ var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=Chicago&id=
 var apiKey = "898b69392d37088b5c01a7c774335e37";
 
 var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&id=60201&units=imperial&APPID=";
-var url1 = forecastURL + apiKey;
+var url2 = forecastURL + apiKey;
 
 var myList;
-var url2 = weatherURL + apiKey;
+var url1 = weatherURL + apiKey;
 
 var weatherServer = "https://alexa-magic-mirror.herokuapp.com/display/current";
 var forecastServer = "https://alexa-magic-mirror.herokuapp.com/display/forecast";
@@ -20,9 +21,9 @@ window.onload = function () {
 
     function displayToday(object) {
         let timeW = moment(object.time);
-        object.lo < 10 ? object.lo = '0' + parseInt(object.lo) : object.lo = parseInt(object.lo)
-        object.hi < 10 ? object.hi = '0' + parseInt(object.hi) : object.hi = parseInt(object.hi)
-        $("#todayandtemp").append(`${object.icon} <span id="today-degree">${parseInt(object.temp)}°</span> <div id="lohi"> <span>lo: ${parseInt(object.lo)}° </span> <span>hi: ${parseInt(object.hi)}° </span></div>`)
+        object.lo < 10 ? object.lo = '0' + parseInt(object.lo) : object.lo = parseInt(object.lo);
+        object.hi < 10 ? object.hi = '0' + parseInt(object.hi) : object.hi = parseInt(object.hi);
+        $("#todayandtemp").append(`${object.icon} <span id="today-degree">${parseInt(object.temp)}°</span> <div id="lohi"> <span>lo: ${parseInt(object.lo)}° </span> <span>hi: ${parseInt(object.hi)}° </span></div>`);
     }
 
     var weatherIcons = {
@@ -35,7 +36,7 @@ window.onload = function () {
         "Atmosphere": '<i class="small-icon wi wi-dog"></i>',
         "Mist": '<i class="small-icon wi wi-day-cloudy"></i>',
         "Haze": '<i class="small-icon wi wi-fog"></i>'
-    }
+    };
 
     function Weather(data) {
         this.time = data.dt_txt;
@@ -46,39 +47,12 @@ window.onload = function () {
         this.icon = weatherIcons[this.weather];
         //this.weather = weatherIcons['data.weather[0].main']
     }
+
     //gets the weather for right now
-    fetch(weatherServer, {
-        method: 'GET'
-    })
-        .then(r => r.json())
-        .then(data => {
-            console.log(data)
+    fetch(url1)
+        .then((prom) => {
+            return prom.json();
         })
-        .catch((err) => console.log(err))
-
-        //Gets the flag
-    fetch(flagsServer, {
-        method: 'GET'
-    })
-        .then(r => r.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch((err) => console.log(err))
-        
-
-    //gets the forecast
-    fetch(forecastServer, {
-        method: 'GET'
-    })
-        .then(r => r.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch((err) => console.log(err))
-
-
-        /*
         .then((data) => {
             console.log(data);
             var today = {};
@@ -89,13 +63,19 @@ window.onload = function () {
             today.weather = data.weather[0].main;
             today.icon = weatherIcons[today.weather];
             displayToday(today);
-            
-        })*/
 
-    
+        })
+        .then(() => {
 
-    /*
-    fetch(url1)
+        })
+        .catch((err) =>
+            console.log(err)
+    );
+
+
+
+
+    fetch(url2)
         .then((data) => data.json())
         .then((prom) => {
             myList = prom.list;
@@ -104,7 +84,7 @@ window.onload = function () {
         .then(() => doStuff(myList))
         .catch((err) =>
             console.log(err)
-        );*/
+        );
 
     function doStuff(apiData) {
         var date = new moment();
@@ -178,10 +158,6 @@ window.onload = function () {
             }
         }
 
-        
-
-
-
         showDaily();
         showHourly();
         showCalendar();
@@ -228,36 +204,37 @@ window.onload = function () {
             //this.weather = weatherIcons['data.weather[0].main']
         }
         var state = 1;
-        $("#todayandtemp").click(function () {
+        $("#time-container").click(function () {
             console.log(myEvents);
 
             toggle(state);
             state++;
             if (state >= 4) state = 0;
         });
-
     }
 
 
 
+
     function getFlags() {
-        var state = 0;
+        console.log("Getting flags");
         setInterval(function () {
-            fetch()
-                .then((prom) => prom.json())
-                .then((data) => {
-                    switch (data) {
+            fetch(flagsServer)
+                .then((data) => data.json())
+                .then((curr) => {
+                    console.log("" + curr.flag);
+                    switch (curr.flag) {
                         case "hide":
                             state = 0;
                             break;
                         case "current":
                             state = 1;
                             break;
-                        case "forecast":
+                        case "forcast":
                             state = 2;
                             break;
-                        case "calendar":
-                            state = 3;
+                        case "schedule":
+                            state = 3; 
                             break;
                         default:
                             state = 0;
@@ -268,6 +245,8 @@ window.onload = function () {
                 });
         }, 1000);
     }
+
+   
 
     function toggle(state) {
         switch (state) {
@@ -308,117 +287,7 @@ window.onload = function () {
         }
     }
 
-}
+    getFlags();
 
-var CLIENT_ID = '123443630640-hod7nh8d4ra5vrv49nb1irodmi0d5ap8.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBorTMiHFRzdz6v8ekiVG02EVNzOAiA0gY';
+};
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
-
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-
-
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        $('#spooky').hide();
-        listUpcomingEvents();
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
-
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-
-var myEvents = [];
-
-function listUpcomingEvents() {
-    gapi.client.calendar.events.list({
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 5,
-        'orderBy': 'startTime'
-    }).then(function (response) {
-        var events = response.result.items;
-        console.log(events);
-
-
-
-        if (events.length > 0) {
-            for (i = 0; i < events.length; i++) {
-                var event = events[i];
-                var when = event.start.dateTime;
-                if (!when) {
-                    when = event.start.date;
-                }
-                console.log(event.summary)
-                console.log(moment(when).format("hh:mm a"));
-                myEvents.push(event)
-            }
-        } else {
-            console.log('No upcoming events found.');
-        }
-    });
-}
